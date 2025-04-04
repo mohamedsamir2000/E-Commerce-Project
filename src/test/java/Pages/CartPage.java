4af839b5d7b578fd64f9a39be6ba54f8fe3f39a9
Marwa <merootreka212@gmail.com>
2025-04-04 11:51:26 -0700

    Cart page edit

--- src/test/java/Pages/CartPage.java
+++ src/test/java/Pages/CartPage.java
@@ -1,6 +1,26 @@
 package Pages;
 
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class CartPage {
+    WebDriver driver;
+    private WebDriverWait wait;
+    public CartPage(WebDriver driver){
+
+        this.driver = driver;
+    }
+    public By CartIcon=By.xpath("//*[@id=\"shopping_cart_container\"]/a");
+    public By CheckoutButton=By.id("checkout");
+    public By ContinueShoppingButton = By.id("continue-shopping");
+    public By FirstNameinput=By.id("first-name");
+    public By LastNameinput=By.id("last-name");
+    public By PostalCode=By.id("postal-code");
+    public By ContinueButton=By.id("continue");
+    public By CancelButton=By.id("cancel");
+    public By FinishButton=By.id("finish");
 
 
 
